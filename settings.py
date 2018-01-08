17c417357e5392da7cb2569eb2b5aaa64c3aa3f9
abhinav_5429 <kumar.abhinav@toppr.com>
2018-01-08 19:41:54 +0530

    Added dummy variables to settings.py

--- settings.py
+++ settings.py
@@ -1,17 +1,18 @@
-# # path to client secret json file for google sheets api to work
-# CLIENT_SECRET_FILE = 'client_secret.json'
+# path to client secret json file for google sheets api to work
+CLIENT_SECRET_FILE = 'client_secret.json'
 #
-# # ids of the spreadsheet that has to be edited
-# SPREADSHEET_IDS = []
+# ids of the spreadsheet that has to be edited
+SPREADSHEET_IDS = []
 #
-# PRICE_ALERT_SPREADSHEET = ''
+PRICE_ALERT_SPREADSHEET = ''
 #
-# CREDENTIALS_DIRECTORY = '~'
+CREDENTIALS_DIRECTORY = '~'
 #
-# SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 #
-# VALID_COINS = ['BTC', 'XRP', 'LTC', 'ETH', 'BCH']
+VALID_COINS = ['BTC', 'XRP', 'LTC', 'ETH', 'BCH']
 #
-# ALARM_YOUTUBE_URL = 'https://www.youtube.com/watch?v=h2uUBbtz2aw'
+ALARM_YOUTUBE_URL = 'https://www.youtube.com/watch?v=h2uUBbtz2aw'
 #
-# SLACK_URL = ''
+SLACK_URL = ''
+
